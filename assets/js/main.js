b40bf8106e432b44ecc5a0df28547871841a8d94
Richard Lindsay <richard@tictocfamily.com>
2018-07-23 15:01:35 +0100

    Basic setup of search

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+console.log("hello")
+
+var sjs = SimpleJekyllSearch({
+  searchInput: document.getElementById('search-input'),
+  resultsContainer: document.getElementById('results-container'),
+  json: '/search.json'
+})
