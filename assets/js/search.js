5b608064a74c92a50be27cccb62fa18c64bf276f
Richard Lindsay <richard@tictocfamily.com>
2018-07-23 20:45:51 +0100

    Tidy up search margins

--- assets/js/search.js
+++ assets/js/search.js
@@ -9,7 +9,6 @@
     json: '/search.json',
     searchResultTemplate: '<article class="post"><img src="{image}" alt="" class="post_image"><h2><a href="{url}">{title}</a></h2><div class="meta"><p class="meta_text">Published on {date}</p></div><a href="{url}" class="big_link" tabindex="-1" aria-hidden="true"></a></article>',
     noResultsText: '<p>No posts found. Sorry</p><img src="https://media1.giphy.com/media/W5YVAfSttCqre/giphy.gif" class="no_results_image" alt="Nope" />',
-    saveState: true,
     fuzzy: true,
     success: function() {
       var all_posts = document.getElementById('posts');
